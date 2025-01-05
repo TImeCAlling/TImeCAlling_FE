769e92467c6e30f1929b0a9de798112b4f7b6c5b
seongh06 <seongh06@naver.com>
2025-01-05 20:18:49 +0900

    [UI/#9] 시간 지정 시 ui 변경

--- app/src/main/java/com/umc/timeCAlling/presentation/addSchedule/AddScheduleFragment.kt
+++ app/src/main/java/com/umc/timeCAlling/presentation/addSchedule/AddScheduleFragment.kt
@@ -2,6 +2,7 @@ package com.umc.timeCAlling.presentation.addSchedule
 
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputFilter
 import android.text.TextWatcher
 import android.view.View
 import android.view.WindowManager
@@ -31,6 +32,7 @@ class AddScheduleFragment: BaseFragment<FragmentAddScheduleBinding>(R.layout.fra
         initDateBottomSheet()
         initTimeBottomSheet()
         initScheduleName()
+        initScheduleMemo()
 
     }
 
@@ -67,6 +69,10 @@ class AddScheduleFragment: BaseFragment<FragmentAddScheduleBinding>(R.layout.fra
         binding.tvAddScheduleDateSave.setOnClickListener { // 저장 버튼 ID를 확인하고 수정해야 할 수 있습니다.
             selectedDate?.let {
                 dateTextView.text = it // TextView에 날짜 설정
+                binding.layoutAddScheduleDate.background = ContextCompat.getDrawable(requireContext(), R.drawable.shape_rect_10_white_fill_mint_line_2) // 배경 변경
+                binding.tvAddScheduleDate.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_900)) // 글자 색깔 변경
+                binding.ivAddScheduleDate.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_calender_check)) // 아이콘 변경
+
                 dateBottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN // BottomSheet 숨기기
             }
         }
@@ -119,6 +125,9 @@ class AddScheduleFragment: BaseFragment<FragmentAddScheduleBinding>(R.layout.fra
 
             val selectedTime = String.format("%02d:%02d", selectedHour, selectedMinute) // 시간 형식 지정
             timeTextView.text = selectedTime // TextView에 시간 설정
+            binding.layoutAddScheduleTime.background = ContextCompat.getDrawable(requireContext(), R.drawable.shape_rect_10_white_fill_mint_line_2) // 배경 변경
+            binding.tvAddScheduleTime.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_900)) // 글자 색깔 변경
+            binding.ivAddScheduleTime.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_calender_check)) // 아이콘 변경
 
             timeBottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN // BottomSheet 숨기기
         }
@@ -151,6 +160,8 @@ class AddScheduleFragment: BaseFragment<FragmentAddScheduleBinding>(R.layout.fra
     }
 
     private fun initScheduleName(){
+        binding.etAddScheduleName.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(10)) // 최대 글자 수 10글자로 제한
+
         binding.etAddScheduleName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // 텍스트 변경 전
@@ -162,12 +173,42 @@ class AddScheduleFragment: BaseFragment<FragmentAddScheduleBinding>(R.layout.fra
 
                 if (textLength > 0) { // 글자가 입력된 경우
                     binding.layoutAddScheduleName.background = ContextCompat.getDrawable(requireContext(), R.drawable.shape_rect_10_white_fill_mint_line_2) // 배경 변경
-                    binding.etAddScheduleName.setTextColor(ContextCompat.getColor(requireContext(), R.color.black)) // 글자 색깔 변경
+                    binding.etAddScheduleName.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_900)) // 글자 색깔 변경
                     binding.ivAddScheduleNameCheck.visibility = View.VISIBLE // 체크 표시 보이기
                 } else { // 글자가 없는 경우
                     binding.layoutAddScheduleName.background = ContextCompat.getDrawable(requireContext(), R.drawable.shape_rect_10_gray250_fill) // 배경 변경
                     binding.etAddScheduleName.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_600)) // 글자 색깔 변경
                     binding.ivAddScheduleNameCheck.visibility = View.INVISIBLE // 체크 표시 숨기기
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // 텍스트 변경 후
+            }
+        })
+
+    }
+
+    private fun initScheduleMemo(){
+        binding.etAddScheduleMemo.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(20)) // 최대 글자 수 10글자로 제한
+
+        binding.etAddScheduleMemo.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // 텍스트 변경 전
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val textLength = s?.length ?: 0 // 입력된 글자 수
+                binding.tvAddScheduleMemoCount.text = textLength.toString() // 글자 수 표시
+
+                if (textLength > 0) { // 글자가 입력된 경우
+                    binding.layoutAddScheduleMemo.background = ContextCompat.getDrawable(requireContext(), R.drawable.shape_rect_10_white_fill_mint_line_2) // 배경 변경
+                    binding.etAddScheduleMemo.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_900)) // 글자 색깔 변경
+                    binding.ivAddScheduleMemoCheck.visibility = View.VISIBLE // 체크 표시 보이기
+                } else { // 글자가 없는 경우
+                    binding.layoutAddScheduleMemo.background = ContextCompat.getDrawable(requireContext(), R.drawable.shape_rect_10_gray250_fill) // 배경 변경
+                    binding.etAddScheduleMemo.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray_600)) // 글자 색깔 변경
+                    binding.ivAddScheduleMemoCheck.visibility = View.INVISIBLE // 체크 표시 숨기기
                 }
             }
 
