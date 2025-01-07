fef93888f44c3886433321c748ea9be79136d1cc
ekgml001 <dahea5069@gmail.com>
2025-01-08 00:19:12 +0900

    [UI/#10] activity_onboarding_spare.xml 구현

---
+++ app/src/main/java/com/umc/timeCAlling/presentation/onboarding/OnboardingSpareActivity.kt
@@ -0,0 +1,19 @@
+package com.umc.timeCAlling.presentation.onboarding
+
+import androidx.activity.viewModels
+import com.umc.timeCAlling.R
+import com.umc.timeCAlling.databinding.ActivityOnboardingSpareBinding
+import com.umc.timeCAlling.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OnboardingSpareActivity : BaseActivity<ActivityOnboardingSpareBinding>(R.layout.activity_onboarding_time) {
+
+    override fun initView() {
+
+    }
+
+    override fun initObserver() {
+
+    }
+}
