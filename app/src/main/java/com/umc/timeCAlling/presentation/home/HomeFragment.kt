cf1eb0b4d48c3031d8cc4cb4e56506d1acb4366b
MinseokChoi <minsuckchoe@naver.com>
2025-01-25 23:29:05 +0900

    [UI/#32] withdraw popup dialog

--- app/src/main/java/com/umc/timeCAlling/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/umc/timeCAlling/presentation/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
