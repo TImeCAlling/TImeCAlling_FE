9aee263222f70da4ca2d64bcd37c0b9224bb5ecc
ekgml001 <dahea5069@gmail.com>
2025-01-28 05:27:23 +0900

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        manifestPlaceholders["kakaoAppKey"] = "@string/kakao_app_key"
     }
 
     buildTypes {
