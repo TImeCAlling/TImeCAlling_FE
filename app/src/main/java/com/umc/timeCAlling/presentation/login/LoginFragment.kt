5f36b2c5892b3f77bdd35b1c4d9b58046eed4bce
ekgml001 <dahea5069@gmail.com>
2025-01-28 14:07:18 +0900

    [Feat/#36] 토큰까지 발급 확인

--- app/src/main/java/com/umc/timeCAlling/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/umc/timeCAlling/presentation/login/LoginFragment.kt
@@ -5,7 +5,6 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.kakao.sdk.user.UserApiClient
 import com.umc.timeCAlling.R
 import com.umc.timeCAlling.databinding.FragmentLoginBinding
 import com.umc.timeCAlling.presentation.base.BaseFragment
@@ -53,63 +52,10 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
 
     private fun loginWithKakao() {
         Log.d("LoginFragment", "loginWithKakao() 호출됨")
-        val context = requireContext()
-
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
-            Log.d("LoginFragment", "카카오톡으로 로그인 가능")
-            loginWithKakaoTalk()
-        } else {
-            Log.d("LoginFragment", "카카오 계정으로 로그인 시도")
-            loginWithKakaoAccount()
-        }
-    }
-
-    private fun loginWithKakaoTalk() {
-        Log.d("LoginFragment", "loginWithKakaoTalk() 호출됨")
-        UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
-            if (error != null) {
-                Log.e("LoginFragment", "카카오톡으로 로그인 실패: ${error.message}")
-                loginWithKakaoAccount()
-            } else if (token != null) {
-                Log.d("LoginFragment", "카카오톡으로 로그인 성공: ${token.accessToken}")
-                handleLoginSuccess(token.accessToken)
-            }
-        }
-    }
-
-    private fun loginWithKakaoAccount() {
-        Log.d("LoginFragment", "loginWithKakaoAccount() 호출됨")
-        UserApiClient.instance.loginWithKakaoAccount(requireContext()) { token, error ->
-            if (error != null) {
-                Log.e("LoginFragment", "카카오 계정으로 로그인 실패: ${error.message}")
-            } else if (token != null) {
-                Log.d("LoginFragment", "카카오 계정으로 로그인 성공: ${token.accessToken}")
-                handleLoginSuccess(token.accessToken)
-            }
-        }
-    }
-
-    private fun handleLoginSuccess(accessToken: String) {
-        Log.d("LoginFragment", "handleLoginSuccess() 호출됨 - AccessToken: $accessToken")
-        UserApiClient.instance.me { user, error ->
-            if (user != null) {
-                val kakaoUserId = user.id.toString()
-                Log.d("LoginFragment", "사용자 정보 요청 성공 - 사용자 ID: $kakaoUserId")
-
-                viewModel.setKakaoUserId(kakaoUserId)
-                viewModel.setAccessToken(accessToken)
-
-                Log.d("LoginFragment", "서버에 로그인 요청 시작")
-                viewModel.loginWithKakaoAccount()
-            } else {
-                Log.e("LoginFragment", "사용자 정보 요청 실패: ${error?.message}")
-                navigateToSignupTermFragment()
-            }
-        }
+        viewModel.loginWithKakao(requireContext())
     }
 
     private fun bottomNavigationRemove() {
-        Log.d("LoginFragment", "bottomNavigationRemove() 호출됨")
         val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.main_bnv)
         bottomNavigationView?.visibility = View.GONE
 
@@ -134,7 +80,6 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
     }
 
     private fun bottomNavigationShow() {
-        Log.d("LoginFragment", "bottomNavigationShow() 호출됨")
         val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.main_bnv)
         bottomNavigationView?.visibility = View.VISIBLE
 
--- app/src/main/java/com/umc/timeCAlling/presentation/login/adapter/LoginViewModel.kt
+++ app/src/main/java/com/umc/timeCAlling/presentation/login/adapter/LoginViewModel.kt
@@ -1,89 +1,85 @@
 package com.umc.timeCAlling.presentation.login.adapter
 
-import android.app.Application
+import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.kakao.sdk.user.UserApiClient
 import com.umc.timeCAlling.domain.model.request.login.KakaoLoginRequestModel
 import com.umc.timeCAlling.domain.model.response.login.KakaoLoginResponseModel
 import com.umc.timeCAlling.domain.repository.LoginRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
-import android.content.Context
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val spf: SharedPreferences,
     private val repository: LoginRepository,
-    application: Application
+    application: android.app.Application
 ) : AndroidViewModel(application) {
-
-    private val _kakaoUserId = MutableLiveData<String>()
-    val kakaoUserId: LiveData<String> get() = _kakaoUserId
 
     private val _loginResult = MutableLiveData<KakaoLoginResponseModel?>()
     val loginResult: LiveData<KakaoLoginResponseModel?> get() = _loginResult
 
-    private val context: Context = application
-
-    private val _accessToken = MutableLiveData<String?>()
-    val accessToken: LiveData<String?> get() = _accessToken
-
-    // accessToken 설정
-    fun setAccessToken(token: String) {
-        _accessToken.value = token
-    }
-
-    // 카카오 사용자 ID 설정
-    fun setKakaoUserId(userId: String) {
-        _kakaoUserId.value = userId
-    }
-
-    // 카카오 웹뷰 로그인 처리
-    fun loginWithKakaoAccount() {
-        val token = _accessToken.value
-        if (token != null) {
-            handleLoginSuccess(token)
+    fun loginWithKakao(context: Context) {
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+            loginWithKakaoTalk(context)
         } else {
-            Timber.e("AccessToken이 설정되지 않았습니다.")
-            _loginResult.postValue(null) // 실패 처리
+            loginWithKakaoAccount(context)
         }
     }
 
-    private fun handleLoginSuccess(kakaoAccessToken: String) {
+    private fun loginWithKakaoTalk(context: Context) {
+        UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+            if (error != null) {
+                Log.e("LoginViewModel", "카카오톡 로그인 실패: ${error.message}")
+                _loginResult.postValue(null)
+            } else if (token != null) {
+                Log.d("LoginViewModel", "카카오톡 로그인 성공: ${token.accessToken}")
+                handleLoginSuccess(token.accessToken)
+            }
+        }
+    }
+
+    private fun loginWithKakaoAccount(context: Context) {
+        UserApiClient.instance.loginWithKakaoAccount(context) { token, error ->
+            if (error != null) {
+                Log.e("LoginViewModel", "카카오 계정 로그인 실패: ${error.message}")
+                _loginResult.postValue(null)
+            } else if (token != null) {
+                Log.d("LoginViewModel", "카카오 계정 로그인 성공: ${token.accessToken}")
+                handleLoginSuccess(token.accessToken)
+            }
+        }
+    }
+
+    private fun handleLoginSuccess(accessToken: String) {
         viewModelScope.launch {
             try {
-                // 로그인 요청
-                val requestModel = KakaoLoginRequestModel(kakaoAccessToken)
+                val requestModel = KakaoLoginRequestModel(accessToken)
                 repository.kakaoLogin(requestModel).onSuccess { response ->
-                    Timber.d("카카오 로그인 성공: ${response.accessToken}")
+                    Log.d("LoginViewModel", "카카오 로그인 성공: ${response.accessToken}")
 
-                    // SharedPreferences에 로그인 정보 저장
                     spf.edit().apply {
                         putString("jwt", response.accessToken)
                         putString("refreshToken", response.refreshToken)
                         putBoolean("isLoggedIn", true)
-                        // putString("nickName", response.nickName)
                         apply()
                     }
 
                     _loginResult.value = response
                 }.onFailure { error ->
-                    Timber.e("카카오 로그인 실패: ${error.message}")
-                    _loginResult.value = null // 실패 처리
+                    Log.e("LoginViewModel", "서버 로그인 실패: ${error.message}")
+                    _loginResult.value = null
                 }
             } catch (e: Exception) {
-                Timber.e("카카오 로그인 중 예외 발생: ${e.message}")
-                _loginResult.value = null // 실패 처리
+                Log.e("LoginViewModel", "서버 요청 중 예외 발생: ${e.message}")
+                _loginResult.value = null
             }
         }
     }
-
-    /*
-    회원가입 부분
-    */
 }
