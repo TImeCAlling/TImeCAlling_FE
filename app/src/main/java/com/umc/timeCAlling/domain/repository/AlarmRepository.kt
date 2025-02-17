db7ccb74ba397e5e8cce133ce1e8f625ede877f8
seongh06 <seongh06@naver.com>
2025-02-17 17:11:49 +0900

    [FEAT/#64] 깨우기 Api 연결 세팅 2

--- app/src/main/java/com/umc/timeCAlling/domain/repository/AlarmRepository.kt
+++ app/src/main/java/com/umc/timeCAlling/domain/repository/AlarmRepository.kt
@@ -1,5 +1,8 @@
 package com.umc.timeCAlling.domain.repository
 
+import com.umc.timeCAlling.domain.model.response.alarm.WakeUpAlarmRequestModel
+import com.umc.timeCAlling.domain.model.response.alarm.WakeUpAlarmResponseModel
+
 interface AlarmRepository {
     suspend fun wakeUpAlarm(requestModel: WakeUpAlarmRequestModel): Result<WakeUpAlarmResponseModel>
 }
